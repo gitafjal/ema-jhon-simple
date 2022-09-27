62528ee4c09d0737ad332c6b7c9e4fe38b081be5
MSJC <ajfalhossen32@gmail.com>
2022-09-27 18:29:35 +0600

    added img

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Product.css';
 const Product = (props) => {
     // console.log(props.product)
-    const { name, img, seller, price, ratings } = props.product;
+    const { name, img, seller, price, ratings, category } = props.product;
     return (
         <div className='product'>
             <img src={img} alt=""></img>
             <p className='productName'> {name} </p>
+            <p> Category: {category}</p>
             <p > Price:$ {price}</p>
             <p> Ratings: {ratings}</p>
             <p> Seller: {seller}</p>
